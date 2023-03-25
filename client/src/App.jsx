e0226ebb4848e6d7e9bad97ba2b8f1bea0d793e4
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-03-25 11:27:50 +0530

    updated the redux state and  routing
    updated the redux state and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
+import HomePage from "Scenes/homePage";
+import LoginPage from "Scenes/loginPage";
+import ProfilePage from "Scenes/profilePage";
 
 const App = () => {
   return (
     <>
-      <div className="app">App</div>
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 };
